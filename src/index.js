ddeda111e5b0d74b9305549f9e85b7b7a35f2459
Zanetti <znti.rafael@gmail.com>
2018-12-19 17:00:44 -0200

    Set device messaging endpoint

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 const iotalib = require('@dojot/iotagent-nodejs');
 const express = require('express');
-
 const util = require('util');
-
 const app = express()
+app.use(express.json());
 
 const iotaPort = 8083;
 
@@ -21,6 +20,18 @@ app.get('/', (req, res) => {
 app.get('/tenants', (req, res) => {
 	let tenants = iota.messenger.tenants;
 	res.status(200).send({tenants});
+});
+
+app.post('/tenants/:tenant/devices/:device/messages', (req, res) => {
+	let {tenant, device} = req.params;
+	let message = req.body;
+
+	let metadata = {
+		timestamp: Date.now(),
+	}
+
+	iota.updateAttrs(device, tenant, message, metadata);
+	res.status(200).send({tenant, device, message, metadata});
 });
 
 app.listen(iotaPort, () => {
