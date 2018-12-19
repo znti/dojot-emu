792fe375affc2c35d26821239f098d34ac0c105b
Zanetti <znti.rafael@gmail.com>
2018-12-19 15:16:07 -0200

    Add /tenants endpoint for tenant list retrieval

--- src/index.js
+++ src/index.js
@@ -10,10 +10,17 @@ const iotaPort = 8083;
 console.log("Initializing custom IoT agent...");
 const iota = new iotalib.IoTAgent();
 iota.init();
-console.log("... custom IoT agent was initialized");
+console.log("... custom IoT agent was initialized", iota);
+
+console.log(iota.messenger.tenants)
 
 app.get('/', (req, res) => {
 	res.status(200).send({message: 'customIoTAgent is on', iota: util.inspect(iota)});
+});
+
+app.get('/tenants', (req, res) => {
+	let tenants = iota.messenger.tenants;
+	res.status(200).send({tenants});
 });
 
 app.listen(iotaPort, () => {
