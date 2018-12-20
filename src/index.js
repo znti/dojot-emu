b18e8c756494f4d33cd3bcec2efa8ebafc5a5de8
Zanetti <znti.rafael@gmail.com>
2018-12-20 15:22:11 -0200

    Start a fresh connection on each request to ensure updated tenants list

--- src/index.js
+++ src/index.js
@@ -7,31 +7,33 @@ app.use(express.json());
 const iotaPort = 8083;
 
 console.log("Initializing custom IoT agent...");
-const iota = new iotalib.IoTAgent();
-iota.init();
-console.log("... custom IoT agent was initialized", iota);
-
-console.log(iota.messenger.tenants)
 
 app.get('/', (req, res) => {
-	res.status(200).send({message: 'customIoTAgent is on', iota: util.inspect(iota)});
+	res.status(200).send({message: 'customIoTAgent is on'});
 });
 
 app.get('/tenants', (req, res) => {
-	let tenants = iota.messenger.tenants;
-	res.status(200).send({tenants});
+	const iota = new iotalib.IoTAgent();
+	iota.init().then(() => {
+		let tenants = iota.messenger.tenants;
+		res.status(200).send({tenants});
+	});
 });
 
 app.post('/tenants/:tenant/devices/:device/messages', (req, res) => {
 	let {tenant, device} = req.params;
 	let message = req.body;
 
-	let metadata = {
-		timestamp: Date.now(),
-	}
-
-	iota.updateAttrs(device, tenant, message, metadata);
-	res.status(200).send({tenant, device, message, metadata});
+	const iota = new iotalib.IoTAgent();
+	iota.init().then(() => {
+	
+		let metadata = {
+			timestamp: Date.now(),
+		}
+	
+		iota.updateAttrs(device, tenant, message, metadata);
+		res.status(200).send({tenant, device, message, metadata});
+	});
 });
 
 app.listen(iotaPort, () => {
